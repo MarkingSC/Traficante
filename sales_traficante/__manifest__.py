648af959d8fe2f1d3884e99081cb2748cb30f6b1
Marco Alejandro <markingsc@MBmarking.local>
2021-08-24 10:37:28 -0500

    [CREATE] - AdiciÂón del módulo para configuraciones de ventas. Adición de validaciones para creacion y modificacion de pedidos. BETA - Puede que tenga errores

---
+++ sales_traficante/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Configuraciones de ventas para Traficante",
+
+    'summary': """
+        Modifica los modelos necesarios para la adaptación al proceso de Traficante""",
+
+    'description': """
+        - Modificación de las funciones de creación y modificación de pedidos de venta para validar que el cliente tenga datos fiscales y de entrega
+        
+    """,
+
+    'author': "Marco Martinez",
+    'website': "www.linkedin.com/in/marco-alejandro-martínez-26b68616b",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Base',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'partner_traficante'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ sales_traficante/models/sale_order.py
@@ -0,0 +1,78 @@
+
+import logging
+from datetime import  datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from odoo import models, fields, api
+
+
+_logger = logging.getLogger(__name__)
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    def _validatePartnerInvDelivData(partners):
+        # Campos requeridos en el cliente para continuar con el pedido
+        reqInvDelivFields = ['street', 'street2', 'city', 'state_id', 'zip', 'country_id', 'mobile', 'email']
+        reqFields = ['vat', 'start_delivery_time', 'finish_delivery_time', 'bank_ids']
+
+        validInvDelivFlag = True
+        validFieldsFlag = True
+
+        for partner in partners:
+            # Revisa si cuenta con todos los campos requeridos
+            for reqField in reqFields:
+                if partner[reqField] is None:
+                    validFieldsFlag = False
+
+            # Revisa si cuenta con todos los campos de facturación y entrega requeridos
+            for reqInvDelivField in reqInvDelivFields:
+                if partner[reqInvDelivField] is None:
+                    validInvDelivFlag = False
+
+            # Si no tiene todos los campos de facturación y entrega entonces busca direcciones asociadas
+            if not validInvDelivFlag:
+                # obtiene las direcciones de facturación y entrega registradas para el cliente
+                invAddresses = self.env['res.partner'].search([('parent_id', '=', partner.id), ('type', '=', 'invoice')])
+                delivAddresses = self.env['res.partner'].search([('parent_id', '=', partner.id), ('type', '=', 'delivery')])
+                _logger.debug("///// DIRECCIONES DE FACTURACION DEL CLIENTE invAddress: " + invAddresses)
+                _logger.debug("///// DIRECCIONES DE ENTREGA DEL CLIENTE delivAddresses: " + delivAddresses)
+
+                # Si no hay alguno de los dos tipos de direcciones
+                if len(pinvAddresses) = 0 or len(delivAddresses) = 0:
+                    validFieldsFlag = False
+
+            if validFieldsFlag:
+                self.env['res.partner'].write({'customer_type', 'A'})
+        return validFieldsFlag
+            
+                
+
+
+    @api.model
+    def create(self, values):
+
+        # Obtiene el objeto del cliente asociado al pedido
+        partnerId = values.partner_id
+        partners = self.env['res.partner'].search([('customer_type','=', 'P'), ('partner_id', '=', partner_id)])
+
+        validFieldsFlag = self._validatePartnerInvDelivData(partners)
+
+        if not validFieldsFlag:
+            raise exceptions.UserError("Capture todos los datos del cliente requeridos para facturación.")
+
+            # Si todo sale bien guarda el pedido
+        return super(SaleOrder, self).create(values)
+
+    @api.multi
+    def write(self, values):
+        for order in self:
+            if 'partner_id' in values:
+                partners = self.env['res.partner'].search([('customer_type','=', 'P'), ('partner_id', '=', values['partner_id'])])
+                validFieldsFlag = self._validatePartnerInvDelivData(partners)
+
+                if not validFieldsFlag:
+                    raise exceptions.UserError("Capture todos los datos del cliente requeridos para facturación.") 
+
+        return super(SaleOrder, self).write(values)
+        
+ 
