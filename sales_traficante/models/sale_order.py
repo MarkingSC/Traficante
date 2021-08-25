73f4137da69909aa9607114659a1bb04368acdef
Marco Alejandro <markingsc@MBmarking.local>
2021-08-24 22:56:42 -0500

    [UPDATE] - Se separó la validacion para cliente principal de la de las direcciones

--- sales_traficante/models/sale_order.py
+++ sales_traficante/models/sale_order.py
@@ -11,15 +11,11 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    def _validatePartnerData(self, partners):
-        _logger.debug("**** INICIA _validatePartnerData")
-        # Campos requeridos en el cliente para continuar con el pedido
-        reqFields = ['vat', 'start_delivery_time', 'finish_delivery_time', 'bank_ids','street', 'street2', 'city', 'state_id', 'zip', 'country_id', 'mobile', 'email']
-
+    def _validateMainPartnerData(self, partners):
+        _logger.debug("**** INICIA _validateMainPartnerData")
+        reqFields = ['vat', 'start_delivery_time', 'finish_delivery_time', 'bank_ids','street']
         validFieldsFlag = True
 
-        #partner_invoice_id
-        #partner_shipping_id
         _logger.debug("**** reqFields: " + str(reqFields))
         for partner in partners:
             _logger.debug("**** ITERA EL PARTNER: " + str(partner.id))
@@ -37,7 +33,36 @@ class SaleOrder(models.Model):
                 _logger.debug("**** ACTUALIZA EL CLIENTE CON CUSTOMER_TYPE A")
                 self.env['res.partner'].write({'customer_type', 'A'})
 
-        _logger.debug("**** FINALIZA _validatePartnerData")
+        _logger.debug("**** FINALIZA _validateAddrPartnerData")
+        return validFieldsFlag
+
+
+    def _validateAddrPartnerData(self, partners):
+        _logger.debug("**** INICIA _validateAddrPartnerData")
+        # Campos requeridos en el cliente para continuar con el pedido
+        reqFields = ['street2', 'city', 'state_id', 'zip', 'country_id', 'mobile', 'email']
+
+        validFieldsFlag = True
+
+        _logger.debug("**** reqFields: " + str(reqFields))
+        for partner in partners:
+            _logger.debug("**** ITERA EL PARTNER: " + str(partner.id))
+            # Revisa si cuenta con todos los campos requeridos
+            for reqField in reqFields:
+                _logger.debug("**** EVALUANDO EL CAMPO: " + str(reqField))
+                _logger.debug("**** partner[reqField]" + str(partner[reqField]))
+                if not partner[reqField]:
+                    _logger.debug("**** NO SE ENCONTRÓ EL CAMPO")
+                    validFieldsFlag = False
+
+                _logger.debug("**** validFieldsFlag: " + str(validFieldsFlag))
+
+            # No lo actualiza porque estas son solo direcciones  
+            #if validFieldsFlag:
+            #    _logger.debug("**** ACTUALIZA EL CLIENTE CON CUSTOMER_TYPE A")
+            #    self.env['res.partner'].write({'customer_type', 'A'})
+
+        _logger.debug("**** FINALIZA _validateAddrPartnerData")
         return validFieldsFlag
             
                 
@@ -45,24 +70,20 @@ class SaleOrder(models.Model):
     def create(self, vals):
 
         # Obtiene el objeto del cliente asociado al pedido
-        partnerInvId = self.partner_invoice_id.id
-        partnerShipId = self.partner_shipping_id.id
+        partnerMain = self.partner_id
         partnerInv = self.partner_invoice_id
         partnerShip = self.partner_shipping_id
 
-
-        #partnerInv = self.env['res.partner'].search([('customer_type','=', 'P'), ('id', '=', partnerInvId)])
-        #partnerInv = self.env['res.partner'].search([('id', '=', partnerInvId)])
-        #partnerShip = self.env['res.partner'].search([('customer_type','=', 'P'), ('id', '=', partnerShipId)])
-        #partnerShip = self.env['res.partner'].search([('id', '=', partnerShipId)])
-
-        validInvPartner = self._validatePartnerData(partnerInv)
-        validShipPartner = self._validatePartnerData(partnerShip)
+        validInvPartner = self._validateAddrPartnerData(partnerInv)
+        validShipPartner = self._validateAddrPartnerData(partnerShip)
+        validMainPartner = self._validateMainPartnerData(partnerMain)
 
         if not validInvPartner:
             raise UserError("Capture todos los datos requeridos para la dirección de facturación.")
         if not validShipPartner:
             raise UserError("Capture todos los datos requeridos para la dirección de entrega.")
+        if not validMainPartner:
+            raise UserError("Capture todos los datos requeridos para el cliente.")
 
         # Si todo sale bien guarda el pedido
         return super(SaleOrder, self).create(vals)
@@ -70,24 +91,25 @@ class SaleOrder(models.Model):
     def write(self, vals):
         for order in self:
             if 'partner_invoice_id' in vals:
-                #partnerInvId = self.partner_invoice_id.id
-                #partnerInv = self.env['res.partner'].search([('customer_type','=', 'P'), ('id', '=', partnerInvId)])
                 partnerInv = self.partner_invoice_id
-                #partnerInv = self.env['res.partner'].search([('id', '=', partnerInvId)])
-                validInvPartner = self._validatePartnerData(partnerInv)
+                validInvPartner = self._validateAddrPartnerData(partnerInv)
 
                 if not validInvPartner:
                     raise UserError("Capture todos los datos requeridos para la dirección de facturación.")
             
             if 'partner_shipping_id' in vals:
-                #partnerShipId = self.partner_shipping_id.id
-                #partnerShip = self.env['res.partner'].search([('customer_type','=', 'P'), ('id', '=', partnerShipId)])
-                #partnerShip = self.env['res.partner'].search([('id', '=', partnerShipId)])
                 partnerShip = self.partner_shipping_id
-                validShipPartner = self._validatePartnerData(partnerShip)
+                validShipPartner = self._validateAddrPartnerData(partnerShip)
 
                 if not validShipPartner:
                     raise UserError("Capture todos los datos requeridos para la dirección de entrega.")
+
+            if 'partner_id' in vals:
+                partnerMain = self.partner_id
+                validMainPartner = self._validateMainPartnerData(partnerMain)
+
+                if not validMainPartner:
+                    raise UserError("Capture todos los datos requeridos para el cliente.")
             
 
         return super(SaleOrder, self).write(vals)
