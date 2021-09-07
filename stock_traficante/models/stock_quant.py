f54d82d793fa16468283f703ef775f8ee275cd0e
Marco Alejandro <markingsc@MBmarking.local>
2021-09-07 14:20:15 -0500

    [UPDATE] - Adicion del campo de categoria del producto a las cantidades de inventario

---
+++ stock_traficante/models/stock_quant.py
@@ -0,0 +1,15 @@
+
+import logging
+from odoo import models, fields, api, _, exceptions
+from datetime import timedelta, datetime
+
+datetime.today()
+
+_logger = logging.getLogger(__name__)
+
+class StockPicking(models.Model):
+    _inherit = 'stock.quant'
+
+    product_category = fields.Many2one(
+        'product.category', 'Category',
+        readonly=True, related='product_id.categ_id')
