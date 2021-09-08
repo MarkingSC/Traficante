c7451ba4f01cdd2d8731bc43f0419aa84f844c64
Marco Alejandro <markingsc@MBmarking.local>
2021-09-08 14:45:37 -0500

    [UPDATE] - adicion de funcion para modificar el display name de contactos

--- partner_traficante/models/res_partner.py
+++ partner_traficante/models/res_partner.py
@@ -140,4 +140,10 @@ class ResPartner(models.Model):
         _logger.debug("**** TERMINA _update_customer_type_from_orders")
         return True
 
+    @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name')
+    def _compute_display_name(self):
+        diff = dict(show_address=None, show_address_only=None, show_email=None, html_format=None, show_vat=None)
+        names = dict(self.with_context(**diff).name_get())
+        for partner in self:
+            partner.display_name = names.get(partner.id) + ' ' + partner.business_name
 
