aa532bae8ea13a0aa8290e4ca033f38f9b1450ea
MarkingSC <martinezsuarezmarcoalejandro@gmail.com>
2021-11-05 08:29:06 -0600

    [UPDATE] - Varios. Adicion de dirección de factura en formulatio de facturas. Modificacion de accion de formulario de creación de contactos para establecer ocmpañía por defecto

--- account_traficante/__manifest__.py
+++ account_traficante/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'partner_traficante'],
+    'depends': ['base', 'account', 'partner_traficante', 'sale'],
 
     # always loaded
     'data': [
