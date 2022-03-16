e3a157977cf55f032157f9a4047554facbb4f43d
MarkingSC <martinezsuarezmarcoalejandro@gmail.com>
2022-03-16 11:21:46 -0600

    [UPDATE] - CORRECCIÓN DE VALIDACION DE CANTIDADES PARA SERVICIOS

--- crm_traficante/models/sale_order.py
+++ crm_traficante/models/sale_order.py
@@ -17,7 +17,7 @@ class saleOrder(models.Model):
                 #_logger.debug("**** qty_available_today" + str(line.qty_available_today)) #Usando esta variable se contempla la cantidad en stock sin restar las reservadas
                 #_logger.debug("**** virtual_available_at_date" + str(line.virtual_available_at_date)) #Usando esta variable se contempla la cantidad en previstas (stock + entradas - salidas)
                 #_logger.debug("**** free_qty_today" + str(line.free_qty_today)) #Usando esta variable solo se contemplan las cantidades que están 100% libres, es decir, stock a mano menos reservadas para pedidos
-                if (line.free_qty_today < line.qty_to_deliver and not line.is_mto):
+                if (line.free_qty_today < line.qty_to_deliver and not line.is_mto and not line.product_id.type == 'service'):
                     pass_flag = False
                     message = _('You plan to sell %s %s of %s but you only have %s %s available in %s warehouse.') % \
                               (line.qty_to_deliver, line.product_uom.name, line.product_id.name, line.free_qty_today,
