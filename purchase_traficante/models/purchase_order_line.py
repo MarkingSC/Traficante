bd8d5ea6ed6095882f7273975621457d4fec0d86
Alejandro-2000 <alejandrocamac500@outlook.com>
2023-12-15 16:20:05 -0600

    Cambios en el informe de productos comprados

--- purchase_traficante/models/purchase_order_line.py
+++ purchase_traficante/models/purchase_order_line.py
@@ -1,6 +1,6 @@
 
 import logging
-from datetime import  datetime, timedelta
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api
 from odoo.exceptions import AccessError, UserError, ValidationError
@@ -24,6 +24,20 @@ class PurchaseOrderLine(models.Model):
             _logger.debug("**** no hay picking_ids en _get_received_date para self:" + str(self))
             self.received_date = None
 
+    @api.model
+    def _get_received_name(self):
+        for record in self:
+            #_logger.info("**** self en _get_received_name: " + str(self))
+            purchase_order = record.order_id
+            #_logger.info("**** purchase_order en _get_received_name: " + str(purchase_order))
+            if purchase_order and len(purchase_order.picking_ids) > 0:
+                stock_move = purchase_order.picking_ids[0]
+                #_logger.info("**** stock_move en _get_received_name: " + str(stock_move))
+                record.received_name = stock_move.name
+            else:
+               # _logger.info("**** no hay picking_ids en _get_received_name para self:" + str(self))
+                record.received_name = None
+
     @api.depends('order_id.receipt_type')
     def _get_receipt_type(self):
         for record in self:
@@ -41,5 +55,18 @@ class PurchaseOrderLine(models.Model):
 
     partner_id = fields.Many2one(string="Proveedor", comodel_name="res.partner", related="order_id.partner_id")
     received_date = fields.Date(string="Fecha de recepción", default=_get_received_date)
+    received_name = fields.Char(string="Nombre de recepción", compute='_get_received_name', store=False)
 
-   
+    origin = fields.Char(string='Documento origen', compute='_get_origin', store=False)
+    elaboration_date = fields.Date(string='Fecha de elaboración', compute='_get_elaboration_date', store=False)
+
+    def _get_origin(self):
+        for line in self:
+            originMap = line.order_id.mapped('name')
+            line.origin = ','.join(originMap)
+
+    def _get_elaboration_date(self):
+        for line in self:
+            dateMap = line.order_id.mapped('date_approve')
+            formattedDates = [date.date().strftime('%Y-%m-%d') for date in dateMap if isinstance(date, datetime)]
+            line.elaboration_date = ','.join(formattedDates)
