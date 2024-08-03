baf949e2ed153ef2e8656f1920edc25da864f5fe
Alejandro-2000 <alejandrocamac500@outlook.com>
2024-08-02 22:11:00 -0600

    Revertir calculo de ieps proceso facturación

--- traficante_sales_detailed_report/models/account_move_line.py
+++ traficante_sales_detailed_report/models/account_move_line.py
@@ -95,14 +95,14 @@ class AccountMoveLine(models.Model):
                 #_logger.info('**** ieps_taxes de la linea: ' + str(ieps_taxes))
 
                 total_ieps = 0
-                #price_unit = record.price_unit
+                price_unit = record.price_unit
                 price_base = record.price_subtotal
                 quantity = record.quantity
                 product = record.product_id
                 customer = record.partner_id
 
                 for tax in ieps_taxes:
-                    compute_all_res = tax.compute_all(price_base, record.currency_id, quantity, product, customer, False, handle_price_include=True) #con estos valores se calcula el amount de los ieps
+                    compute_all_res = tax.compute_all(price_unit, record.currency_id, quantity, product, customer, False, handle_price_include=True) #con estos valores se calcula el amount de los ieps
                     #_logger.info('**** compute_all_res de la linea: ' + str(compute_all_res))
                     
                     for calc_tax in compute_all_res['taxes']:
