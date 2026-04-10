32fa665b66c93b5c72aa8c9118eb45fb3c309e26
Marco Alejandro Martinez Suarez <gitlab@masbyte.com>
2026-04-10 10:14:48 -0600

    UPDATE - ADICION DE RESTRICCION DE ALMACEN SOBRE REPORTE DE INVENTARIO

--- stock_traficante/models/stock_quant.py
+++ stock_traficante/models/stock_quant.py
@@ -13,3 +13,13 @@ class stockQuant(models.Model):
     product_category = fields.Many2one(
         'product.category', 'Category', store=True,
         readonly=True, related='product_id.categ_id')
+
+    warehouse_id = fields.Many2one(
+        'stock.warehouse', 'Warehouse', store=True, compute='_get_quant_warehouse_id')
+
+    def _get_quant_warehouse_id(self):
+        for quant in self:
+            location = quant.location_id
+            if location:
+                warehouse = self.env['stock.warehouse'].search([('lot_stock_id', '=', location.id)], limit=1)
+                quant.warehouse_id = warehouse.id if warehouse else False
